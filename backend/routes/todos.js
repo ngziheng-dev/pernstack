30cbf7f128289ef4a925b35e991a3e7c4a906b1c
ngziheng-dev <ziheng467@gmail.com>
2026-01-30 17:29:20 +0800

    pernstack deployment

---
+++ backend/routes/todos.js
@@ -0,0 +1,105 @@
+import { Router } from "express";
+import pool from "../db.js";
+
+const router = Router();
+
+router.post("/", async (req, res) => {
+
+    try {
+
+        const { description, completed } = req.body;
+        if (!description) {
+            return res.status(400).json({ error: "Description is required" })
+        }
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *",
+            [description, completed || false]
+        );
+        res.json(newTodo.rows[0]);
+
+    }
+
+    catch (e) {
+
+        console.error(e.message);
+        res.status(500).send("Server Error")
+
+    }
+
+})
+
+router.get("/", async (req, res) => {
+
+    try {
+
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+
+    }
+
+    catch (e) {
+
+        console.error(e.message);
+        res.status(500).send("Server Error")
+
+    }
+
+})
+
+router.put("/:id", async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+        const { description, completed } = req.body;
+        if (!description) {
+            return res.status(400).json({ error: "Description is required" })
+        }
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1, completed = $2 WHERE todo_id = $3 RETURNING *",
+            [description, completed || false, id]
+        );
+        if (updateTodo.rows.length === 0) {
+            return res.status(400).json({ error: "Todo not found" })
+        }
+        
+        res.json({
+            message: "Todo was updated",
+            todo: updateTodo.rows[0]
+        });
+
+    }
+
+    catch (e) {
+
+        console.error(e.message);
+        res.status(500).send("Server Error")
+
+    }
+
+})
+
+router.delete("/:id", async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1 RETURNING *", [id]);
+        if (deleteTodo.rows.length === 0) {
+            return res.status(400).json({ error: "Todo not found" })
+        }
+        
+        res.json("Todo was deleted!");
+
+    }
+
+    catch (e) {
+
+        console.error(e.message);
+        res.status(500).send("Server Error")
+
+    }
+
+})
+
+export default router
